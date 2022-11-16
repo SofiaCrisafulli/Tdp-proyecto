d9405cbc56c8d3def6763651037ba84a538354b8
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2022-11-16 11:31:25 -0300

    Add files via upload

---
+++ LogicaAbstracta/VisitorAbstracto.java
@@ -0,0 +1,29 @@
+package LogicaAbstracta;
+
+import Logica.Jugador;
+
+public abstract class VisitorAbstracto extends Thread{
+
+	protected Personaje miPj;
+
+	public void aceptarJugador(Jugador j) {
+
+	}
+
+	public void aceptarEnemigo(Enemigo ene) {
+
+	}
+
+	public void aceptarPremio(Premio p) {
+
+	}
+
+	public void aceptarProyectil(Proyectil L) {
+
+	}
+
+	public void aceptarProyectilEnemigo(ProyectilEnemigo p) {
+
+	}
+
+}
